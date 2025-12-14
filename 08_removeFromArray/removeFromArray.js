f33a20de90219a89813c51ba5b34a457b9a80ffa
DanielDarsamo <28514526+DanielDarsamo@users.noreply.github.com>
2025-12-14 15:11:48 +0200

    Implement reverseString and removeFromArray functions with proper parameters and logic

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array,number) {
+ let arpa=[];
+ for(let i=0; i<array.length;i++){
+    if(array[i]!= number){
+        arpa.push(array[i]);
+    }
+ }
+ return arpa;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
