fc95ba2f389ed9ea319c78878d6d64ead8357c2b
DanielDarsamo <28514526+DanielDarsamo@users.noreply.github.com>
2026-01-08 14:19:55 +0200

    Refactor sumAll, leapYears, and temperature conversion functions to include parameters for input values

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1,num2 ) {
+    let start;
+    let end;
+    let sum =0;
+    if (num1> num2){
+        end=num1;
+        start=num2;
+    }
+    else if(num1< num2){
+        end=num2;
+        start=num1;
+        
+    };
 
+    for(let i= start; i<=end; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  const celsius = (temp - 32) * (5 / 9);
+  return Math.round(celsius * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temps) {
+  const fah=temps*(9/5)+32;
+  return Math.round(fah*10)/10;
+
 };
 
 // Do not edit below this line
